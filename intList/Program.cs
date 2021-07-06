b985a7ef3bd058725c2cd94d04058e7ca5d416c3
Hamlet <danielhamlet123@gmail.com>
2021-07-05 22:52:09 -0400

    Función Add

---
+++ intList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace intList
+{
+    class IntList
+    {
+        private int[] data;
+        private int length;
+        public IntList()
+        {
+            data = new int[] {};
+            length = 0;
+        }
+
+        public void Add(int num)
+        {
+            int [] array = new int [length + 1];
+            data.CopyTo(array,0);
+            data = array;
+            length++;  
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntList lista = new IntList();
+
+            lista.Add(5);   
+        }
+    }
+}
