8d1502f191c87be2fb268294e6ea241babf2b6d2
Hamlet <danielhamlet123@gmail.com>
2021-07-05 23:51:12 -0400

    IntSet

---
+++ IntSet/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace IntSet
+{
+    class IntSet
+    {
+        private int[] data;
+        private int length;
+        public IntSet()
+        {
+            data = new int[] {};
+            length = 0;
+        }
+
+        
+        
+        public bool Contains(int num)
+        {
+            foreach(int i in data)
+            {
+                if (i == num)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        public int Length()
+        {
+            return length;
+        }
+        public int[] All()
+        {
+            return data;
+        }
+        public bool isEmpty()
+        {
+            if (length == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
